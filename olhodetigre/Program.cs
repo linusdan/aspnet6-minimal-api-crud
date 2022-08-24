c48047ba6c608f836598a70db7a18fa8f20ce3b2
Danilo Santos <artededan@protonmail.ch>
2022-08-24 12:01:08 -0300

    (Minimal API) Melhorando a Swagger UI

--- olhodetigre/Program.cs
+++ olhodetigre/Program.cs
@@ -1,11 +1,15 @@
 using olhodetigre.Data;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Hello Minimal API :)", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,11 +17,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(swaggerUIOptions =>
+    {
+        swaggerUIOptions.DocumentTitle = "Hello Minimal API :)";
+        swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API serving a very simple Post model");
+        swaggerUIOptions.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
 
-app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync());
+app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync())
+    .WithTags("Posts Endpoints");
 
 app.Run();
