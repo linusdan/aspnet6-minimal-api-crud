2539e3d618ef7ef294e2748db9bd89004a4f28f7
Danilo Santos <artededan@protonmail.ch>
2022-08-24 11:21:21 -0300

    (Minimal API) First API Endpoint

--- olhodetigre/Program.cs
+++ olhodetigre/Program.cs
@@ -1,3 +1,5 @@
+using olhodetigre.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,5 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync());
 
 app.Run();
